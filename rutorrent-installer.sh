4ce2c5323e70f52ceb9a690d83954830787dc20b
nwgat <github@nwgat.net>
2016-04-22 18:07:37 +0200

    Update rutorrent-installer.sh

--- rutorrent-installer.sh
+++ rutorrent-installer.sh
@@ -19,10 +19,6 @@ wget https://github.com/mholt/caddy/releases/download/v0.8.2/caddy_linux_amd64.t
 tar xvf caddy_linux_amd64.tar.gz caddy >> /dev/null
 install caddy /usr/bin
 
-# small fixes like starting supvisor on startup and caddy on port 80/443
-sed '/exit 0/i setcap cap_net_bind_service=+ep /usr/bin/caddy' /etc/rc.local -i.bkp
-sed '/exit 0/i supervisord -c /etc/supervisor/supervisord.conf' /etc/rc.local -i.bkp
-
 # setup rtorrent
 useradd -m -p --disabled-password -s /bin/bash rtorrent
 su -c 'mkdir $HOME/.session/ $HOME/rtdl $HOME/.caddy' rtorrent
@@ -30,16 +26,24 @@ su -c 'mkdir $HOME/.session/ $HOME/rtdl $HOME/.caddy' rtorrent
 cp conf/Caddyfile /home/rtorrent/.caddy/Caddyfile
 cp conf/rtorrent.rc /home/rtorrent/.rtorrent.rc
 chown -R rtorrent:rtorrent /home/rtorrent/.caddy/Caddyfile
-chown -R rtorrent:rtorrent /home/rtorrent/rtorrent.rc
+chown -R rtorrent:rtorrent /home/rtorrent/.rtorrent.rc
 
 # ruTorrent
 git clone https://github.com/Novik/ruTorrent /home/rtorrent/ruTorrent
 chown -R rtorrent:rtorrent /home/rtorrent/ruTorrent
 
+# small fixes like starting supvisor on startup and caddy on port 80/443
+sed '/exit 0/i setcap cap_net_bind_service=+ep /usr/bin/caddy' /etc/rc.local -i.bkp
+sed '/exit 0/i supervisord -c /etc/supervisor/supervisord.conf' /etc/rc.local -i.bkp
+
+# allow caddy for port 80 and start supervisord
+setcap cap_net_bind_service=+ep /usr/bin/caddy
+supervisord -c /etc/supervisor/supervisord.conf
+
 # Details
 echo "Login Details"
 echo ""
-supervisord -c /etc/supervisor/supervisord.conf
+
 supervisorctl status
 echo ""
 echo "Username: admin"
